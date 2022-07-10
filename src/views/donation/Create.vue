5ccd5daccbfd348bbd81ef3b5d05e5a18fc4afa2
arief sangga <arif.sangga@gmail.com>
2022-07-10 21:04:27 +0700

    update tombol donasi sekarang

--- src/views/donation/Create.vue
+++ src/views/donation/Create.vue
@@ -1,11 +1,97 @@
 <template>
-    <div class="pt-20 pb-20 text-center">
-        halaman buat donasi
+    <div class="pt-20 pb-20">
+        <div class="container grid grid-cols-1 p-3 mx-auto sm:w-full md:w-5/12">
+            <div class="p-5 bg-white rounded-md shadow-md">
+                <div class="text-xl">
+                    Masukkan Nominal Donasi
+                </div>
+                <div class="mt-2 mb-3 border-2 border-gray-200"></div>
+
+                <div class="mb-2">
+                    <label class="mt-2 text-lg font-bold">Rp.</label>
+                    <input type="number"
+                        class="w-full p-2 mt-2 text-xl text-right placeholder-gray-600 bg-gray-200 border border-gray-200 rounded shadow-sm appearance-none h-15 focus:outline-none focus:placeholder-gray-600 focus:bg-white focus-within:text-gray-600" v-model="donation.amount"
+                    >
+                </div>
+
+                <div class="mb-2">
+                    <label class="mt-2 text-lg font-bold">Do'a</label>
+                    <textarea rows="3"  v-model="donation.pray"
+                        class="w-full p-5 mt-2 placeholder-gray-600 bg-gray-200 border border-gray-200 rounded shadow-sm appearance-none focus:outline-none focus:placeholder-gray-600 focus:bg-white focus-within:text-gray-600" placeholder="Tulis Do'a/Ucapan">
+                    </textarea>
+                </div>
+
+                <button @click="storeDonation" class="w-full mt-4 text-base font-bold uppercase bg-yellow-500 rounded-md shadow-md focus:outline-none focus:bg-yellow-600">LANJUT PEMBAYARAN</button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'DonationCreateComponent'
-}
+    //hook vue
+    import { reactive } from 'vue'
+    
+    //hook vuex
+    import { useStore } from 'vuex'
+    
+    //hook vue router
+    import { useRoute, useRouter } from 'vue-router'
+    
+    //hook Toast
+    import { useToast } from "vue-toastification"
+
+    export default {
+        name: 'DonationCreateComponent',
+
+        setup() {
+            //store vuex
+            const store = useStore()
+
+            //route
+            const route = useRoute()
+
+            //router
+            const router = useRouter()
+
+            //toast
+            const toast = useToast()
+
+            //state donation
+            const donation = reactive({
+                amount: 0,                      // <-- data nilai donasi
+                pray: '',                       // <-- data kata-kata/doa
+                campaignSlug: route.params.slug // <-- data "slug" dari campaign
+            })
+
+            // console.log(donation);
+
+            //method store donation
+            function storeDonation() {
+
+                //check minimal donasi
+                if(donation.amount < 10000) {
+                    toast.error('Donasi Minimal Rp. 10.000')
+                    return false
+                }
+
+                store.dispatch('donation/storeDonation', donation)
+                .then(() => {
+
+                    toast.success('Transaksi Berhasil Dibuat!')
+
+                    //redirect ke dashboard
+                    router.push({name: 'donation.index'})
+
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+            }
+
+            return {
+                donation,
+                storeDonation,
+            }
+        }
+    }
 </script>
