b8c04c25cf6e36af40467ecd715c1865072a77f5
arief sangga <arif.sangga@gmail.com>
2022-07-11 16:31:01 +0700

    update status apu httpps

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const Api = axios.create({
     // set endpoint default api
-    baseURL: 'http://donasiapps.utama.appdev.my.id/api'
+    baseURL: 'https://donasiapps.utama.appdev.my.id/api'
 })
 
 export default Api
