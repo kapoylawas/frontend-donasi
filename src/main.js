c7ba63c2ffe3fd90646d3a8936363e5ca45de52b
arief sangga <arif.sangga@gmail.com>
2022-08-31 12:57:41 +0700

    upda .js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 // Added by the CLI
-import './registerServiceWorker';
+import './registerServiceWorker.js';
 
 createApp(App).use(router).mount('#app');
 
